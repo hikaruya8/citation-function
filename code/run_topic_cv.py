15e2dba573d97c12b2a8d3da19f5859b16ffc06a
hikaru-y <>
2018-12-09 13:45:15 +0900

    arrange folder

--- code/run_topic_cv.py
+++ code/run_topic_cv.py
@@ -12,7 +12,7 @@ import re
 import numpy
 import numpy as np
 from numpy import histogram
-import cPickle
+import pickle
 from scipy.stats import pearsonr
 from sklearn.linear_model import LogisticRegression
 from nltk.tokenize.punkt import PunktSentenceTokenizer as sentTokenizer
@@ -72,7 +72,7 @@ if len(sys.argv) < 2:
 
 feature_dir = sys.argv[1]
 
-print 'Loading topic vectors'
+print('Loading topic vectors')
 citation_id_to_topic_vecs = {}
 with open('../working-dir/topics/citance.doc-topics.txt') as citance_topic_f:
     with open('../working-dir/topics/extended-citance.doc-topics.txt') as extended_topic_f:
@@ -90,17 +90,17 @@ with open('../working-dir/topics/citance.doc-topics.txt') as citance_topic_f:
                 if t_citation_id != citation_id.replace('.', ''):
                     raise Exception('BAD TOPICS: %s != %s' % (citation_id, t_citation_id))
 
-                citance_topic_vec = np.array(map(float, citance_topic_line[:-1].split('\t')[2:]))
-                extended_topic_vec = np.array(map(float, extended_topic_line[:-1].split('\t')[2:]))
+                citance_topic_vec = np.array(list(map(float, citance_topic_line[:-1].split('\t')[2:])))
+                extended_topic_vec = np.array(list(map(float, extended_topic_line[:-1].split('\t')[2:])))
                 
                 citation_id_to_topic_vecs[citation_id] = (citance_topic_vec, extended_topic_vec)
                 
                 if (line_no+1) % 10000 == 0:
-                    print 'loaded %d lines' % (line_no+1)
+                    print('loaded %d lines' % (line_no+1))
                 if (line_no+1) % 100000 == 0:
                     break
 
-print 'Loaded %d topic vectors' % (len(citation_id_to_topic_vecs))
+print('Loaded %d topic vectors' % (len(citation_id_to_topic_vecs)))
 
 x = []
 y = []
@@ -113,7 +113,7 @@ count = 0
 
 for fname in os.listdir(feature_dir):
     if '.ftr' not in fname:
-        print 'skipping ' + fname
+        print('skipping ' + fname)
         continue
     f = open(feature_dir + fname, "r")
     citing_paper_id = fname[0:8]
@@ -131,7 +131,7 @@ for fname in os.listdir(feature_dir):
 
         # print citation_id
         if citation_id not in citation_id_to_topic_vecs:
-            print 'skipping!'
+            print('skipping!')
             continue
 
         if cite_function == 'Unknown':
@@ -148,7 +148,7 @@ for fname in os.listdir(feature_dir):
 
         class_ = cite_function
 
-        feature_vec = map(float, cols[4:])
+        feature_vec = list(map(float, cols[4:]))
         topic_vecs = citation_id_to_topic_vecs[citation_id]
         feature_vec = np.concatenate([feature_vec, topic_vecs[0], topic_vecs[1]])
         #feature_vec = np.concatenate([topic_vecs[0], topic_vecs[1]])
@@ -170,14 +170,14 @@ for fname in os.listdir(feature_dir):
         paper_indices.append(to_append)
 
 
-print set(y)
+print(set(y))
 
 x = numpy.array(x)
 y = numpy.array(y)
 
 
 
-print x.shape, y.shape, len(paper_indices)
+print(x.shape, y.shape, len(paper_indices))
 
 # print x.shape[1]/2
 
@@ -201,7 +201,7 @@ ALL_Y_REAL = []
 
 # pe_debug = open('cv.cite-func.all-features.tsv', 'w')
 
-print 'beginning training'
+print('beginning training')
 
 #for idx in range(10):
 for idx in range(TOTAL_PAPERS):
@@ -248,27 +248,27 @@ for idx in range(TOTAL_PAPERS):
     macro_r = recall_score(ALL_Y_REAL, ALL_Y_PREDICTED, labels=labels, average='macro')
     micro_r = recall_score(ALL_Y_REAL, ALL_Y_PREDICTED, labels=labels, average='micro')
 
-    print 'Running accuracy after %d papers, %f macro F1 (P: %f, R: %f), %f micro F1 (P: %f, R: %f)' \
-        % (idx+1, macro_f1, macro_p, macro_r, micro_f1, micro_p, micro_r)
+    print('Running accuracy after %d papers, %f macro F1 (P: %f, R: %f), %f micro F1 (P: %f, R: %f)' \
+        % (idx+1, macro_f1, macro_p, macro_r, micro_f1, micro_p, micro_r))
     
-    print labels
-    print sklearn.metrics.confusion_matrix(ALL_Y_REAL, ALL_Y_PREDICTED, \
-        labels=labels)
-    print '---------------------\n\n'
+    print(labels)
+    print(sklearn.metrics.confusion_matrix(ALL_Y_REAL, ALL_Y_PREDICTED, \
+        labels=labels))
+    print('---------------------\n\n')
 
 # pe_debug.close()
   
-print "---"
-print clf
+print("---")
+print(clf)
 #print "ACCURACY", numpy.mean(accscores)
 
-print "Macro F1: score", f1_score(ALL_Y_REAL, ALL_Y_PREDICTED, labels=labels, average='macro')
+print("Macro F1: score", f1_score(ALL_Y_REAL, ALL_Y_PREDICTED, labels=labels, average='macro'))
 
-print ' '.join(labels)
+print(' '.join(labels))
 #print ' '.join(['Positional', 'Essential'])
 
 
-print sklearn.metrics.confusion_matrix(ALL_Y_REAL, ALL_Y_PREDICTED, labels=labels)
+print(sklearn.metrics.confusion_matrix(ALL_Y_REAL, ALL_Y_PREDICTED, labels=labels))
 
 # labels=['Background', 'Motivation', 'Prior', 'Uses',  'Extends', 'CompareOrContrast', 'Future'])
     
